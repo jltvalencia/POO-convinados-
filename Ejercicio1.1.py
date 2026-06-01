d8923549ee1003bdc655a1aa4e9f8829304b7322
jltvalencia <jlt.valencia@yavirac.edu.ec>
2026-05-31 23:25:10 -0500

    Ejercicoo convinado 1.1

---
+++ Ejercicio1.1.py
@@ -0,0 +1,32 @@
+class Dispositivo:
+    def __init__(self,marca,precio):
+        self.marca=marca
+        self.precio=precio 
+    def encender(self):
+        print(f"El dispositivo {self.marca} se encendio correctamente ")
+    def mostrar_precio(self):
+        print(f"El precio de tu dispositivo es de {self.precio}")
+class celular (Dispositivo):
+    def __init__(self, marca, precio,operador):
+        super().__init__(marca, precio)
+        self.operador=operador
+    def encender (self):
+        print(f"Tu telefono con la operdadora {self.operador} fue encendido correctamente ")
+
+dispositivos = [
+    Dispositivo("Genérico", 100),
+    celular("Samsung", 500, "Claro")
+]
+for disp in dispositivos:
+    print("Dispositivo:", disp.marca)
+    
+    disp.encender()
+    
+    disp.mostrar_precio()
+    print()
+        
+   
+    
+        
+        
+        
